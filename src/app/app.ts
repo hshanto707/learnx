7060ccb5c881759db42e9e485b1167da88e52207
Hasibul Hasan <hshanto707@gmail.com>
2023-12-22 05:18:32 +0600

    app updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,41 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors';
-import { UserRoutes } from './modules/user/user.route';
 import router from './routers';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
+import { notFound } from './middlewares/notFound';
 
-const app = express()
+const app: Application = express()
+
 app.use(express.json())
+
 app.use(cors())
 
 app.use('/api/v1', router)
 
-app.use(ErrorCom)
-
-// app.use('/api/users', UserRoutes);
-
-// app.get('/', (req: Request, res: Response) => {
-//   res.json({
-//     success: true,
-//     message: 'Api worked successfully'
-//   });
-// });
-
-// app.all('*', (req: Request, res: Response) => {
-//   res.status(400).json({
-//     success: false,
-//     message: 'API not found!',
-//   });
-// })
-
-// app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-//   if (error) {
-//     res.status(404).json({
-//       success: false,
-//       message: 'Something went wrong!',
-//     })
-//   }
-// })
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app;
